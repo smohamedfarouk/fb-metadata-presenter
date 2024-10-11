fb282a1ebf49e1393d4e5431cda7922047ccc2ab
smohamedfarouk <smohamedfarouk@yahoo.com>
2024-10-11 18:55:53 +0500

    Revert "Fix dropdown2"
    This reverts commit c6bd50e029ee68b09610f77643fe8e4acb036762.

--- app/models/metadata_presenter/page_answers.rb
+++ app/models/metadata_presenter/page_answers.rb
@@ -37,7 +37,7 @@ module MetadataPresenter
       elsif component && component.type == 'address'
         address_answer(method_name.to_s)
       elsif component && component.type == 'dropdown'
-        answers[method_name.to_s].to_a
+        dropdown_answer(component.id)
       else
         sanitize(answers[method_name.to_s])
       end
